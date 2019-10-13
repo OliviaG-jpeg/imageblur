1bd6e366031181d69d2223ad6c4d4ce9b83d78c2
Olivia Goehrig <goehrigo@gmail.com>
2019-10-13 23:06:39 +0000

    blur 2

--- imageblur1.rb
+++ imageblur1.rb
@@ -2,6 +2,35 @@ class Image
   def initialize(array)
     @image = array
   end
+
+  def add_ones
+    ones = []
+    @image.each_with_index do |row, row_number|
+      row.each_with_index do |item, col_number|
+        if item == 1
+          ones << [row_number, col_number]
+        end
+      end
+    end
+    ones
+  end
+
+  def blur!
+    ones = add_ones
+    @image.each_with_index do |row, row_number|
+      row.each_with_index do |item, col_number|
+        ones.each do |found_row_number, found_col_number|
+          if row_number == found_row_number && col_number == found_col_number
+            @image[row_number -1][col_number] = 1 unless row_number == 0 
+            @image[row_number +1][col_number] = 1 unless row_number >= 3
+            @image[row_number][col_number -1] = 1 unless col_number == 0
+            @image[row_number][col_number +1] = 1 unless col_number >= 3
+          end
+        end
+      end
+    end
+  end
+
   def output_image
     @image.each do |row|
       puts row.join
@@ -11,8 +40,11 @@ end
 
 image = Image.new([
       [0, 0, 0, 0],
+      [0, 0, 0, 0],
       [0, 1, 0, 0],
-      [0, 0, 0, 1],
       [0, 0, 0, 0]
     ])
+image.output_image
+puts
+image.blur!
 image.output_image
